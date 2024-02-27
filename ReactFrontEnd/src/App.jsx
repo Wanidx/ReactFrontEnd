8f024a3cba5a1e4871860defd1d01ea4a69a182f
Wanidx <s6506021610045@kmutnb.ac.th>
2024-02-27 10:02:17 +0700

    Propstype

--- ReactFrontEnd/src/App.jsx
+++ ReactFrontEnd/src/App.jsx
@@ -1,11 +1,21 @@
+import Contact from "./assets/Contact"
+import Hello from "./assets/Hello"
+
 function App() {
+  const HelloData = [
+    { name : 'Anirach', message : 'Hi there' },
+    { name : 'Tom', message : 'Hello...' }
+  ];
+
   return (
       <div>
-        <h1>Hello World</h1>
-        <p>Hello World</p>
-      </div>
+        {HelloData.map((data, index) => (
+          <Hello key={index} name={data.name} message={data.message} />
+        ))}
 
-  )
+        <Contact email="Anirach@gmail.com" phone="0817320731" />
+      </div>
+  );
 }
 
 export default App
